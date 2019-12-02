a57fcc69e6bdac5af1315fc03288bfeb505c345c
Yatin Kalra <yatin.kalra22@gmail.com>
2019-12-02 12:06:04 +0530

    initial push of project

---
+++ precache-manifest.acb4227056403c08e132a1843694de9b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bff30a93ddc57ce19bae5c77a30a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a85054e2fb630645988",
+    "url": "/static/css/main.cdc6ad73.chunk.css"
+  },
+  {
+    "revision": "9852f7a7dcf6ccce89b7",
+    "url": "/static/js/2.fae5154d.chunk.js"
+  },
+  {
+    "revision": "2a85054e2fb630645988",
+    "url": "/static/js/main.3d513058.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "926281180e2704f167139c48306230cc",
+    "url": "/static/media/avatar-png-1.92628118.png"
+  },
+  {
+    "revision": "99c75a252c58bf0115acb9aea9184a83",
+    "url": "/static/media/like.99c75a25.svg"
+  },
+  {
+    "revision": "79af482615b9e61b7a4ec049057162b7",
+    "url": "/static/media/search_icon_pr2sma.79af4826.svg"
+  },
+  {
+    "revision": "9d1c42a4c783d311d34f2a8395b65491",
+    "url": "/static/media/upload.9d1c42a4.svg"
+  }
+]);
